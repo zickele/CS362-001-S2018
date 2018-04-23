0d72855f8321a260a74f26b750d0ceee265158f6
zickele <zickele@oregonstate.edu>
2018-04-23 12:52:17 -0700

    Added assignment-2

---
+++ projects/zickele/Assignment-2/Calendar/README.md
@@ -0,0 +1,1 @@
+The Calendar is a Java application that provides classes and methods for storing diffirent appointments.
---
+++ projects/zickele/Assignment-2/Calendar/src/test/java/calendar/DataHandlerTest.java
@@ -0,0 +1,28 @@
+
+/** A JUnit test class to test the class DataHandler. */
+
+
+package calendar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import calendar.Appt;
+import calendar.CalDay;
+import calendar.DataHandler;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+
+public class DataHandlerTest{
+
+  @Test(timeout = 4000)
+  public void test00()  throws Throwable  {
+
+  }
+  @Test(timeout = 4000)
+  public void test01()  throws Throwable  {
+
+  }
+
+}
